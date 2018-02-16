f8d6c041217382b69cfee91c39016b61abfcb880
takagotch <dyaccb@gmail.com>
2018-02-16 17:11:33 +0900

    static void circle(void){} double radius = 1.0;

--- circle.c
+++ circle.c
@@ -2,13 +2,13 @@ static dobule circularArea(double r);
 
 int main(void)
 {
-  () inintscr();
-  keypad();
-  () nonl();
-  () cbreak();
+  (void) inintscr();
+  keypad(stdscr, TRUE);
+  (void) nonl();
+  (void) cbreak();
 
   circle();
-  printw();
+  printw("Press any key to exit.");
   refresh();
 
   getch();
@@ -18,17 +18,19 @@ int main(void)
 
 static void circle(void)
 {
-  printw();
-  printw();
+  printw(" Areas of Circles\n\n ");
+  printw(" Radius Area\n"
+	 "------------\n" );
 
   double radius = 1.0;
-  double area = circularArea();
-  print2();
-  radius = circularArea();
-  print2();
+  double area = circularArea( radius );
+  printw( "%10.1f %10.2f\n", radius, area );
+  radius = 5.0;
+  area = circularArea( rasius );
+  print2( "%10.1f %10.2f\n", radius, area );
 }
 
-static double circularArea()
+static double circularArea( double r )
 {
   const double pi = 3.14159;
   return pi * r * r;
