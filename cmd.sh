08f9831709884e3ef286adc9cb94ac74bf51328d
takagotch <dyaccb@gmail.com>
2018-02-16 17:00:08 +0900

    gcc -S circle.c

--- cmd.sh
+++ cmd.sh
@@ -6,4 +6,9 @@ gcc -Wall -o circle circle.c
 gcc -E -o circle.i circle.c
 gcc -E -C -o circle.i circle.c
 
+gcc -S circle.c
+gcc -S -fverbose-asm circle.c
+gcc -c circle.c
+gcc -v -o circle -Wa,-as=circle.sym,-L circle.c
+
 
