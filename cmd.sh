2b484fb5243eb54c872963c22874b42dc28e244c
takagotch <dyaccb@gmail.com>
2018-02-16 17:14:27 +0900

    gcc -lncurses -@l, -Map,circle.map circle.c circulararea.c

--- cmd.sh
+++ cmd.sh
@@ -13,5 +13,16 @@ gcc -v -o circle -Wa,-as=circle.sym,-L circle.c
 
 gcc -g -o circle -Wa,-a=circle.list,-L circle.c
 
+gcc -o circle circle.c -lncurses
+gcc -o circle circle.c /usr/local/lib/libncurses.a
+
+gcc -o circle -L/local/lib -lncurses circle.c
+gcc -lncurses -Wl,-Map,circle.map circle.c circulararea.c
+
+gcc -c circle.c
+gcc -c circulararea.c
+gcc -c circle.c circulararea.c
+
+
 
 
