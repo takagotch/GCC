915529bbecf36e14b9f16165168d31e5baee7c1b
takagotch <dyaccb@gmail.com>
2018-03-06 03:23:19 +0900

    gcc -Werror -std=c99 gcc -Werror -std=c99 -c -o circulararea.o circulararea.c

--- cmd.sh
+++ cmd.sh
@@ -23,6 +23,28 @@ gcc -c circle.c
 gcc -c circulararea.c
 gcc -c circle.c circulararea.c
 
+gcc -o circle circle.o circulararea.o -lncurses
+
+gcc -p bigprg mainpart.c -x assembler trickypart.asm -x otherpart.c
+
+gcc -c circulararea.c
+gcc -shared -o libcirclararea.so circularrea.o
+
+gcc -c circle.c
+
+gcc -ocircle circle.o libcirculararea.so -lncurses
+
+gcc -static - ocircle circle.o circulararea.o -lncurses
+
+gcc -o circle circle.o circulararea.o /usr/lib/libncurses.a
+
+gcc -Wall -03 -o circle circle.c circulararea.c -lncurses
+
+gcc -Wall -03 -fno-inline-functions -o circle circle.c \
+circulararea.c -lncurses
+
+gcc -Wall -0 -march=athlon-4 -o circle circle.c circulararea.c \
+-lncurses
 
 
 
